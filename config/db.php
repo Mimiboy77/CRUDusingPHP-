b4e1af9dcfb35ff78827a35b8bf2f8b449f9f925
unknown <miraclebassey28@gmail.com>
2024-12-11 15:50:09 +0100

    this is to connect to a Database

---
+++ config/db.php
@@ -0,0 +1,4 @@
+<?php $conn = mysqli_connect('localhost', 'users', '123456789', 'myUserbio');
+   if (!$conn) {
+      echo 'connectin error: '; 
+   } ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php 
+include ('config/db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Users details</title>
+</head>
+<body>
+    This is the Home page 
+</body>
+</html>
