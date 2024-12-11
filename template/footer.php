0ec2b2215e8a9702a819060cd0d5b228062330e6
unknown <miraclebassey28@gmail.com>
2024-12-11 16:34:33 +0100

    added: Navbar&footer using includes

---
+++ template/footer.php
@@ -0,0 +1,5 @@
+<footer class="section">
+    <div class="center grey-text">copyright  2024 Miracle</div>
+</footer>
+
+</body>
---
+++ template/header.php
@@ -0,0 +1,29 @@
+<head>
+<title>my pizza</title>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<style>
+    .brand{
+        background-color: green !important;
+    }
+    .brand-text{
+        color: green !important;
+    }
+    form{
+        max-width: 460px;
+        margin: 20px auto;
+        padding: 20px;
+    }
+</style>
+</head>
+<body class="grey lighten-4 ">
+    <nav class="white z-depth-0 ">
+        <div class="container">
+            <a href="index.php" class="brand-logo brand-text">LOGO</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="add.php" class="btn brand z-depth-0">Add users</a></li>
+            </ul>
+        </div>
+    </nav>
